cd61f0777332e00f916dd522e7bf50eb3d8fb543
Joel Shin <joel@fluxxlabs.com>
2022-08-28 10:01:25 -0700

    created routes; api v1 restaurants controller

---
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -0,0 +1,57 @@
+module Api
+  module V1
+    class RestaurantsController < ApplicationController
+      def show
+        restaurant = Restaurant.find_by(slug: params[:slug])
+
+        render json: RestaurantSerializer.new(restaurant, options).serialized_json
+      end
+
+      def index
+        restaurants = Restaurant.all
+
+        render json: RestaurantSerializer.new(restaurants, options).serialized_json
+      end
+
+      def create
+        restaurant = Restaurant.new(restaurant_params)
+
+        if restaurant.save
+          render json: RestaurantSerializer.new(restaurant).serialized_json
+        else
+          render json: { error: restaurant.errors.messages }, status: 422
+        end
+      end
+
+      def update
+        restaurant = Restaurant.find_by(slug: params[:slug])
+
+        if restaurant.update(restaurant_params)
+          render json: RestaurantSerializer.new(restaurant, options).serialized_json
+        else
+          render json: { error: restaurant.errors.messages }, status: 422
+        end
+      end
+
+      def destroy
+        restaurant = Restaurant.find_by(slug: params[:slug])
+
+        if restaurant.destroy
+          head :no_content
+        else
+          render json: { errors: restaurant.errors.messages }, status: 422
+        end
+      end
+
+      private
+
+      def options
+        @options ||= { include: %i[reviews] }
+      end
+
+      def restaurant_params
+        params.require(:restaurant).permit(:name, :image_url, :location)
+      end
+    end
+  end
+end
