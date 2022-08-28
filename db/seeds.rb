a7c947e3fa1d6596e8da6df400ad8a4b1fda5310
Joel Shin <joel@fluxxlabs.com>
2022-08-27 22:10:17 -0700

    seed reviews too

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create([
+  {
+    name: "Joe's Pizzeria",
+    image_url: "https://unsplash.com/photos/L8V5XU7-MR4",
+    location: "nyc"
+  },
+  {
+    name: "Pizzeria Delfina",
+    image_url: "https://unsplash.com/photos/exSEmuA7R7k",
+    location: "SF, CA"
+  }
+])
+
+r = Restaurant.where(location: 'nyc').first
+Review.create([
+  {
+    title: "amazing",
+    description: "classic nyc pizza, the best",
+    score: 10,
+    restaurant_id: r.id
+  },
+  {
+    title: "very good sauce",
+    description: "not enough cheese",
+    score: 9,
+    restaurant_id: r.id
+  },
+])
