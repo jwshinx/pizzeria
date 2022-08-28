69129fa223520679a944b1e392e69ae306a1df45
Joel Shin <joel@fluxxlabs.com>
2022-08-28 10:26:12 -0700

    endpoints work

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class RestaurantsController < ApplicationController
+      protect_from_forgery with: :null_session
+
       def show
         restaurant = Restaurant.find_by(slug: params[:slug])
 
