372cb1c48d1af691ae0649037ed22a418321c253
Joel Shin <joel@fluxxlabs.com>
2022-08-27 21:59:20 -0700

    ran model generator for restaurant and review; ran db migrate

---
+++ db/migrate/20220828043443_create_restaurants.rb
@@ -0,0 +1,12 @@
+class CreateRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :image_url
+      t.string :location
+      t.string :slug
+
+      t.timestamps
+    end
+  end
+end
