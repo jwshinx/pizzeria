cd8e44e2ffb26b5890f772c67a2a98b3fd9fa7f1
Joel Shin <joel@fluxxlabs.com>
2022-08-28 23:01:47 -0700

    fixed warning re ReactDOM.render no longer supported in react 18

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App = () => {
-  return (<div>Hello from react</div>)
+  return (<div>Hello from App.js</div>)
 }
 
 export default App
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
 import App from '../components/App'
+import { createRoot } from 'react-dom/client';
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <App />,
-    document.body.appendChild(document.createElement('div'))
-  )
-})
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
