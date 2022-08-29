b814c23e6c1fe67bbb42f03ba92ad66ac1284359
Joel Shin <joel@fluxxlabs.com>
2022-08-28 23:15:21 -0700

    unable to resolve *ReactDOM.render* issue for layout erb

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -3,8 +3,10 @@
 // of the page.
 
 import React from 'react'
+// import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+// import { createRoot } from 'react-dom/client';
 
 const Hello = props => (
   <div>Hello {props.name}!</div>
@@ -17,6 +19,21 @@ Hello.defaultProps = {
 Hello.propTypes = {
   name: PropTypes.string
 }
+
+// Warning: ReactDOM.render is no longer supported in React 18.
+// https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
+// this doesnt work
+// function AppWithCallbackAfterRender() {
+//   useEffect(() => {
+//     console.log('rendered!!');
+//   });
+
+//   return <Hello name="React" />
+// }
+
+// const container = document.createElement('div');
+// const root = createRoot(container);
+// root.render(<AppWithCallbackAfterRender />);
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
