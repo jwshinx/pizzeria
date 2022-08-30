bd5b4fc9404146f00626a433c20e1467720466fe
Joel Shin <joel@fluxxlabs.com>
2022-08-30 09:22:46 -0700

    added restaurant#show link to list jsx page

--- app/javascript/components/restaurants/Restaurant.jsx
+++ app/javascript/components/restaurants/Restaurant.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Restaurant = (props) => {
   console.log("+++> props.item:", props.item)
-  const { name, image_url, location } = props.item
+  const { name, image_url, location, slug } = props.item
 
   return (
     <div className='p-2'>
-      <div className='text-lg'>{name}</div>
+      <div className='text-lg'>
+        <Link to={`/restaurants/${slug}`}>
+          {name}
+        </Link>
+      </div>
       <div>{location}</div>
       <div>
         <img src={image_url} alt={image_url}/>
