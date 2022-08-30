f4df8fba557d4b5c3457f3af0491b32d513b1d05
Joel Shin <joel@fluxxlabs.com>
2022-08-29 19:55:05 -0700

    installed axios; renders fetched restaurants data

--- app/javascript/components/restaurants/Restaurants.jsx
+++ app/javascript/components/restaurants/Restaurants.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
 
 const Restaurants = () => {
+  const [restaurants, setRestaurants] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/v1/restaurants.json')
+      .then(resp => {
+        setRestaurants(resp.data.data)
+      })
+      .catch(err => console.log("+++> err:", err))
+  }, [restaurants.length])
+
+  const list = restaurants.map(item => {
+    return (
+      <li key={item.attributes.name}>
+        {item.attributes.name}
+      </li>
+    )
+  })
   return (
-    <div>
-      This is restaurants#index view.
-    </div>
+    <Fragment>
+      <div className="header">
+        <h1>Restaurants</h1>
+        <div className="subheader">Food and conversation.</div>
+      </div>
+      <div className="grid">
+        <ul>{list}</ul>
+      </div>
+    </Fragment>
   )
 }
 
