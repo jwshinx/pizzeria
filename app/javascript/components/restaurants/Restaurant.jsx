2eea88ed5ab9acac60ad9eee0399eb82de55b1c0
Joel Shin <joel@fluxxlabs.com>
2022-09-07 10:40:42 -0700

    image covers width and height, looks good

--- app/javascript/components/restaurants/Restaurant.jsx
+++ app/javascript/components/restaurants/Restaurant.jsx
@@ -6,15 +6,18 @@ const Restaurant = (props) => {
   const { name, image_url, location, slug } = props.item
 
   return (
-    <div className='bg-red-50'>
-      <div className='text-lg hover:text-blue-500'>
-        <Link to={`/restaurants/${slug}`} state={{ color: 'red' }}>
-          {name}
-        </Link>
-      </div>
-      <div>{location}</div>
-      <div>
-        <img className='object-cover h-48 w-96' src={image_url} alt={image_url}/>
+    <div className='relative group bg-red-50'>
+      <img className='object-cover h-full w-96' src={image_url} alt={image_url}/>
+      <div
+        className='absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40'
+      >
+        <div class="flex justify-between w-full hover:text-blue-500">
+          <div class="font-normal">
+            <p class="text-sm">{name}</p>
+            <p class="text-sm">{location}</p>
+            <p class="text-xs">245 likes - 109 Shares</p>
+          </div>
+        </div>
       </div>
     </div>
   )
