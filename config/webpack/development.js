9bca1b8730fe8929134eeeb0826470d334c55d58
Joel Shin <joel@fluxxlabs.com>
2022-09-07 21:24:12 -0700

    ran rails-webpacker-install-typescript; installed fork-ts-checker-webpack-plugin; removed prop-types

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,25 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+// process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+// const environment = require('./environment')
+// module.exports = environment.toWebpackConfig()
 
+// installed: yarn add fork-ts-checker-webpack-plugin --dev
+// so need to modify this file
+// see: https://firxworx.com/blog/coding/configuring-ruby-on-rails-to-support-react-typescript-with-webpacker/
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
+
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const path = require('path')
+
+environment.plugins.append(
+  'ForkTsCheckerWebpackPlugin',
+  new ForkTsCheckerWebpackPlugin({
+    typescript: {
+      configFile: path.resolve(__dirname, '../../tsconfig.json'),
+    },
+    // non-async type checking will block compilation on error
+    async: false,
+  }),
+)
 
 module.exports = environment.toWebpackConfig()
